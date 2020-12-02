65627f65c82b53af30727c0d4938ebc3cc9b41d9
gud388 <gud388@hotmail>
2020-12-03 09:37:39 +1300

    updating for transfer of file

--- funny_functions_in_C++.c
+++ funny_functions_in_C++.c
@@ -4,9 +4,10 @@ using namespace std;
 
 // function declaration
 int func_num(int num1,int num2);
-int common_factor(long num1, long num2, long i); 
-int func_greatest_common_factor(int num1, int num2); 
-
+int func_common_factor(long num1, long num2, long i); 
+int func_greatest_common_factor(int num1, int num2);
+int func_factorial(int num1, int num2);
+ 
 int func_num(int num1,int num2) {
     if(num1 > num2) {
         cout << num1 << " is bigger than " << num2 << endl;
@@ -17,27 +18,34 @@ int func_num(int num1,int num2) {
     }
 }
 
-long common_factor(long num1, long num2) {
-    if(num1 == 1)
-    return 1;
-    if(num1 % num2 == 0)
-    return num1 + common_factor(num1,num2 - 1);
-    return common_factor(num1, num2 - 1);
+long func_common_factor(long num1, long num2) {
+    if(num1 == 1) {
+        return 1;
+    }
+    if(num1 % num2 == 0) {
+        cout << num1 << " is a factor" << num2 << " is a factor" << endl; 
+        return func_common_factor(num1, num2 % num1);
+    }
 }
 
 int func_greatest_common_factor(int num1,int num2) {
-    if (num2 == 0)
-    return num1;
-    return func_greatest_common_factor(num2, num1 % num2); 
+    if (num2 == 0) {
+        return num1;
+        return func_greatest_common_factor(num2, num1 % num2);
+    } 
+}
+
+int func_factorial(int fact_num) {
+    if(fact_num <= 1)  
+        return 1;
+    else {
+        return fact_num*func_factorial(fact_num-1);
+    }
 }
 
 int main() {
-    int callingfunc;
-    int gcd;
-    int common;
-    long num1;
-    long num2;
-    long i;
+    int callingfunc, gcd, common, factorial_num1, factorial_num2;
+    long num1, num2, i;
 
     char err[] = "unable to read";
 
@@ -57,9 +65,15 @@ int main() {
     
     callingfunc = func_num(num1, num2);
 
-    common = common_factor(num1, i);
-    cout << "A common factor is " << common << endl;
+    //common = func_common_factor(num1, i);
+    //cout << "A common factor is " << common << endl;
 
     gcd = func_greatest_common_factor(num1, num2);
     cout << "The greatest commom factor is " << gcd << endl;
+
+    factorial_num1 = func_factorial(num1);
+    cout << "The factorials for the first value are " << factorial_num1 << endl;
+
+    factorial_num2 = func_factorial(num2);
+    cout << "The factorials for the second value are " << factorial_num2 << endl;
 }

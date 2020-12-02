2852220136c43374de45342945b8c833fcccd5ff
gud388 <gud388@hotmail>
2020-12-03 09:38:08 +1300

    Transfer of file

--- Understanding C++(input&output) .c
+++ Understanding C++(input&output) .c
@@ -4,7 +4,8 @@ using namespace std;
 
 // function declaration
 int func_num(int num1,int num2);
-int func_common_factor(int num1, int num2); 
+int common_factor(long num1, long num2, long i); 
+int func_greatest_common_factor(int num1, int num2); 
 
 int func_num(int num1,int num2) {
     if(num1 > num2) {
@@ -16,17 +17,27 @@ int func_num(int num1,int num2) {
     }
 }
 
-int func_common_factor(int num1,int num2) {
+long common_factor(long num1, long num2) {
+    if(num1 == 1)
+    return 1;
+    if(num1 % num2 == 0)
+    return num1 + common_factor(num1,num2 - 1);
+    return common_factor(num1, num2 - 1);
+}
+
+int func_greatest_common_factor(int num1,int num2) {
     if (num2 == 0) 
     return num1;
-    return func_common_factor(num2, num1 % num2);
+    return func_greatest_common_factor(num2, num1 % num2); 
 }
 
 int main() {
-    int num1;
-    int num2;
     int callingfunc;
     int gcd;
+    int common;
+    long num1;
+    long num2;
+    long i;
 
     char err[] = "unable to read";
 
@@ -44,8 +55,11 @@ int main() {
     cout << "The first value is " << num1 << endl;
     cout << "The second value is " << num2 << endl;
     
-    callingfunc = func_num(num1,num2);
-    gcd = func_common_factor(num1,num2);
+    callingfunc = func_num(num1, num2);
 
-    cout << "The commom factor is " << gcd << endl;
+    common = common_factor(num1, i);
+    cout << "A common factor is " << common << endl;
+
+    gcd = func_greatest_common_factor(num1, num2);
+    cout << "The greatest commom factor is " << gcd << endl;
 }
